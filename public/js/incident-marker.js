b1a609242f6798651572d21f1100dc99e628bf6b
Pradyum Mistry <39883432+AltF4-Games@users.noreply.github.com>
2024-03-16 20:49:52 +0530

    Fix Side Panel

--- public/js/incident-marker.js
+++ public/js/incident-marker.js
@@ -1,1 +1,111 @@
-function IncidentMarker(t){this.onSelected=t.onSelected,this.iconsMapping=t.iconsMapping,this.incidentSeverity=t.incidentSeverity,this._marker=null}IncidentMarker.prototype._validateSeverity=function(t){return t&&t.magnitudeOfDelay},IncidentMarker.prototype._validateIconCategory=function(t){return t&&t.iconCategory||"Unknown"},IncidentMarker.prototype._validateEvent=function(t){return t&&t.events},IncidentMarker.prototype._validateTime=function(t){return t&&window.formatters.formatToDurationTimeString(t.delay)},IncidentMarker.prototype._validateDistance=function(t){return t.length&&window.formatters.formatAsMetricDistance(t.length)},IncidentMarker.prototype._validateRoadNumber=function(t){return t&&t.roadNumbers},IncidentMarker.prototype._validateAndParseEndTime=function(t){var e=t&&t.endTime;if(e)return window.Formatters.formatToDateTimeStringForTrafficIncidents(e)},IncidentMarker.prototype._createPopupHeader=function(){return["Category","Streets","Length","Est. end time"].map((function(t){return"<div>"+t+"</div>"})).join("")},IncidentMarker.prototype._createPopupContent=function(t){var e=t.properties,i=this._validateSeverity(e),n=this._validateIconCategory(e),r=this._validateEvent(e),a=this._validateTime(e),o=this._validateDistance(e),d=this._validateRoadNumber(e),c=this._validateAndParseEndTime(e),s=DomHelpers.elementFactory("div");return s.innerHTML='<div class="tt-traffic-details"><div class="tt-traffic-details__header"><div class="tt-traffic-icon"><div class="tt-icon-circle-'+this.incidentSeverity[i]+'"><div class="tt-icon-'+this.iconsMapping[n]+'"></div></div></div>'+(d?'<div class="tt-road-shield">'+d+"</div>":"")+(r?'<div class="tt-incident-category">'+r[0].description+"</div>":"")+'</div></div><div class="tt-traffic-description">'+(a?'<div class="tt-incident-delay">'+("No delay"===a?"<b>"+a+"</b>":"<b>Delay: </b>"+a)+"</div>":"")+(o?'<div class="tt-incident-length"><b>Traffic length: </b>'+o+"</div>":"")+(c?'<div class="tt-incident-end-time"><b>Estimated end time: </b>'+c+"</div>":"")+"</div></div>",s},IncidentMarker.prototype.markerFactory=function(t){var e=document.createElement("div");e.className="tt-icon-marker tt-icon-circle-"+this.incidentSeverity[t.properties.magnitudeOfDelay];var i=new tt.Popup({offset:15}).setMaxWidth("none"),n=document.createElement("div");n.className="tt-icon-marker-inner";var r=new tt.Marker({anchor:"center",element:e}).setLngLat({lng:t.geometry.coordinates[0],lat:t.geometry.coordinates[1]});return n.className+=" tt-icon-"+this.iconsMapping[t.properties.iconCategory],e.appendChild(n),i.setDOMContent(this._createPopupContent(t)),r.setPopup(i),r.getPopup().on("open",this.onSelected.bind(window,t.properties.id)),this._marker=r,r},IncidentMarker.prototype.update=function(t){if(this._marker){this._marker.setLngLat(t.geometry.coordinates);var e=this._createPopupContent(t);this._marker.getPopup()&&this._marker.getPopup().setDOMContent(e)}},IncidentMarker.prototype.getMarker=function(){return this._marker},window.IncidentMarker=window.IncidentMarker||IncidentMarker;
+function IncidentMarker(t) {
+  (this.onSelected = t.onSelected),
+    (this.iconsMapping = t.iconsMapping),
+    (this.incidentSeverity = t.incidentSeverity),
+    (this._marker = null);
+}
+(IncidentMarker.prototype._validateSeverity = function (t) {
+  return t && t.magnitudeOfDelay;
+}),
+  (IncidentMarker.prototype._validateIconCategory = function (t) {
+    return (t && t.iconCategory) || "Unknown";
+  }),
+  (IncidentMarker.prototype._validateEvent = function (t) {
+    return t && t.events;
+  }),
+  (IncidentMarker.prototype._validateTime = function (t) {
+    return t && window.formatters.formatToDurationTimeString(t.delay);
+  }),
+  (IncidentMarker.prototype._validateDistance = function (t) {
+    return t.length && window.formatters.formatAsMetricDistance(t.length);
+  }),
+  (IncidentMarker.prototype._validateRoadNumber = function (t) {
+    return t && t.roadNumbers;
+  }),
+  (IncidentMarker.prototype._validateAndParseEndTime = function (t) {
+    var e = t && t.endTime;
+    if (e)
+      return window.Formatters.formatToDateTimeStringForTrafficIncidents(e);
+  }),
+  (IncidentMarker.prototype._createPopupHeader = function () {
+    return ["Category", "Streets", "Length", "Est. end time"]
+      .map(function (t) {
+        return "<div>" + t + "</div>";
+      })
+      .join("");
+  }),
+  (IncidentMarker.prototype._createPopupContent = function (t) {
+    var e = t.properties,
+      i = this._validateSeverity(e),
+      n = this._validateIconCategory(e),
+      r = this._validateEvent(e),
+      a = this._validateTime(e),
+      o = this._validateDistance(e),
+      d = this._validateRoadNumber(e),
+      c = this._validateAndParseEndTime(e),
+      s = DomHelpers.elementFactory("div");
+    return (
+      (s.innerHTML =
+        '<div class="tt-traffic-details"><div class="tt-traffic-details__header"><div class="tt-traffic-icon"><div class="tt-icon-circle-' +
+        this.incidentSeverity[i] +
+        '"><div class="tt-icon-' +
+        this.iconsMapping[n] +
+        '"></div></div></div>' +
+        (d ? '<div class="tt-road-shield">' + d + "</div>" : "") +
+        (r
+          ? '<div class="tt-incident-category">' + r[0].description + "</div>"
+          : "") +
+        '</div></div><div class="tt-traffic-description">' +
+        (a
+          ? '<div class="tt-incident-delay">' +
+            ("No delay" === a ? "<b>" + a + "</b>" : "<b>Delay: </b>" + a) +
+            "</div>"
+          : "") +
+        (o
+          ? '<div class="tt-incident-length"><b>Traffic length: </b>' +
+            o +
+            "</div>"
+          : "") +
+        (c
+          ? '<div class="tt-incident-end-time"><b>Estimated end time: </b>' +
+            c +
+            "</div>"
+          : "") +
+        "</div></div>"),
+      s
+    );
+  }),
+  (IncidentMarker.prototype.markerFactory = function (t) {
+    var e = document.createElement("div");
+    e.className =
+      "tt-icon-marker tt-icon-circle-" +
+      this.incidentSeverity[t.properties.magnitudeOfDelay];
+    var i = new tt.Popup({ offset: 15 }).setMaxWidth("none"),
+      n = document.createElement("div");
+    n.className = "tt-icon-marker-inner";
+    var r = new tt.Marker({ anchor: "center", element: e }).setLngLat({
+      lng: t.geometry.coordinates[0],
+      lat: t.geometry.coordinates[1],
+    });
+    return (
+      (n.className +=
+        " tt-icon-" + this.iconsMapping[t.properties.iconCategory]),
+      e.appendChild(n),
+      i.setDOMContent(this._createPopupContent(t)),
+      r.setPopup(i),
+      r.getPopup().on("open", this.onSelected.bind(window, t.properties.id)),
+      (this._marker = r),
+      r
+    );
+  }),
+  (IncidentMarker.prototype.update = function (t) {
+    if (this._marker) {
+      this._marker.setLngLat(t.geometry.coordinates);
+      var e = this._createPopupContent(t);
+      this._marker.getPopup() && this._marker.getPopup().setDOMContent(e);
+    }
+  }),
+  (IncidentMarker.prototype.getMarker = function () {
+    return this._marker;
+  }),
+  (window.IncidentMarker = window.IncidentMarker || IncidentMarker);
--- public/js/incidents-details-manager.js
+++ public/js/incidents-details-manager.js
@@ -1,1 +1,105 @@
-function IncidentDetailsMarkersManager(e,t){this.incidentMarkers={},this.map=e,this.incidentMarkerFactory=t,this.addMarkers=this.addMarkers.bind(this),this._addMarker=this._addMarker.bind(this),this._mapResponseToFeaturesDictionary=this._mapResponseToFeaturesDictionary.bind(this),this.getMarkers=this.getMarkers.bind(this)}function IncidentsDetailsManager(e,t,n){this.map=e,this.services=t,this.options=n,this.onDetailsUpdated=this.options.onDetailsUpdated,this.incidentsMarkerManager=new IncidentDetailsMarkersManager(e,n.incidentMarkerFactory),this._fetchIncidentDetails=this._fetchIncidentDetails.bind(this),this._registerEvents=this._registerEvents.bind(this),this._updateIncidentMarkers=this._updateIncidentMarkers.bind(this),this._updateIncidentMarkers(),this._registerEvents()}IncidentDetailsMarkersManager.prototype.addMarkers=function(e){var t=this._mapResponseToFeaturesDictionary(e);Object.keys(this.incidentMarkers).forEach(function(e){t[e]?(this.incidentMarkers[e].update(t[e]),delete t[e]):(this.incidentMarkers[e].getMarker().remove(),delete this.incidentMarkers[e])}.bind(this)),Object.values(t).forEach(function(e){this._addMarker(e)}.bind(this))},IncidentDetailsMarkersManager.prototype._addMarker=function(e){var t=e.properties.id,n=this.incidentMarkerFactory();n.markerFactory(e),this.incidentMarkers[t]=n,n.getMarker().addTo(this.map)},IncidentDetailsMarkersManager.prototype._mapResponseToFeaturesDictionary=function(e){return e.incidents.reduce((function(e,t){var n={};return t.geometry.type="Point",t.geometry.coordinates=t.geometry.coordinates[0],n[t.properties.id]=t,Object.assign(e,n)}),Object.create(null))},IncidentDetailsMarkersManager.prototype.getMarkers=function(){var e={};return Object.keys(this.incidentMarkers).forEach(function(t){e[t]=this.incidentMarkers[t].getMarker()}.bind(this)),e},IncidentsDetailsManager.prototype._fetchIncidentDetails=function(){var e={key:this.options.key,boundingBox:this.map.getBounds(),fields:"{\n            incidents {\n                type,\n                geometry {\n                    type,\n                    coordinates\n                },\n                properties {\n                    id,\n                    iconCategory,\n                    magnitudeOfDelay,\n                    events {\n                        description,\n                        code,\n                        iconCategory\n                    },\n                    startTime,\n                    endTime,\n                    from,\n                    to,\n                    length,\n                    delay,\n                    roadNumbers,\n                    aci {\n                        probabilityOfOccurrence,\n                        numberOfReports,\n                        lastReportTime\n                    }\n                }\n            }\n        }"};return this.services.incidentDetailsV5(e)},IncidentsDetailsManager.prototype._registerEvents=function(){var e=window.debounce(function(e){"vectorTilesIncidents"===e.sourceId&&this._updateIncidentMarkers()}.bind(this),500);this.map.on("sourcedata",e),this.map.on("moveend",this._updateIncidentMarkers),this.map.on("zoomend",this._updateIncidentMarkers)},IncidentsDetailsManager.prototype._updateIncidentMarkers=function(){this.map.getZoom()>9&&this._fetchIncidentDetails().then(function(e){this.incidentsMarkerManager.addMarkers(e),this.onDetailsUpdated({trafficIncidents:e,markers:this.incidentsMarkerManager.getMarkers()})}.bind(this))},window.IncidentsDetailsManager=window.IncidentsDetailsManager||IncidentsDetailsManager;
+function IncidentDetailsMarkersManager(e, t) {
+  (this.incidentMarkers = {}),
+    (this.map = e),
+    (this.incidentMarkerFactory = t),
+    (this.addMarkers = this.addMarkers.bind(this)),
+    (this._addMarker = this._addMarker.bind(this)),
+    (this._mapResponseToFeaturesDictionary =
+      this._mapResponseToFeaturesDictionary.bind(this)),
+    (this.getMarkers = this.getMarkers.bind(this));
+}
+function IncidentsDetailsManager(e, t, n) {
+  (this.map = e),
+    (this.services = t),
+    (this.options = n),
+    (this.onDetailsUpdated = this.options.onDetailsUpdated),
+    (this.incidentsMarkerManager = new IncidentDetailsMarkersManager(
+      e,
+      n.incidentMarkerFactory
+    )),
+    (this._fetchIncidentDetails = this._fetchIncidentDetails.bind(this)),
+    (this._registerEvents = this._registerEvents.bind(this)),
+    (this._updateIncidentMarkers = this._updateIncidentMarkers.bind(this)),
+    this._updateIncidentMarkers(),
+    this._registerEvents();
+}
+(IncidentDetailsMarkersManager.prototype.addMarkers = function (e) {
+  var t = this._mapResponseToFeaturesDictionary(e);
+  Object.keys(this.incidentMarkers).forEach(
+    function (e) {
+      t[e]
+        ? (this.incidentMarkers[e].update(t[e]), delete t[e])
+        : (this.incidentMarkers[e].getMarker().remove(),
+          delete this.incidentMarkers[e]);
+    }.bind(this)
+  ),
+    Object.values(t).forEach(
+      function (e) {
+        this._addMarker(e);
+      }.bind(this)
+    );
+}),
+  (IncidentDetailsMarkersManager.prototype._addMarker = function (e) {
+    var t = e.properties.id,
+      n = this.incidentMarkerFactory();
+    n.markerFactory(e),
+      (this.incidentMarkers[t] = n),
+      n.getMarker().addTo(this.map);
+  }),
+  (IncidentDetailsMarkersManager.prototype._mapResponseToFeaturesDictionary =
+    function (e) {
+      return e.incidents.reduce(function (e, t) {
+        var n = {};
+        return (
+          (t.geometry.type = "Point"),
+          (t.geometry.coordinates = t.geometry.coordinates[0]),
+          (n[t.properties.id] = t),
+          Object.assign(e, n)
+        );
+      }, Object.create(null));
+    }),
+  (IncidentDetailsMarkersManager.prototype.getMarkers = function () {
+    var e = {};
+    return (
+      Object.keys(this.incidentMarkers).forEach(
+        function (t) {
+          e[t] = this.incidentMarkers[t].getMarker();
+        }.bind(this)
+      ),
+      e
+    );
+  }),
+  (IncidentsDetailsManager.prototype._fetchIncidentDetails = function () {
+    var e = {
+      key: this.options.key,
+      boundingBox: this.map.getBounds(),
+      fields:
+        "{\n            incidents {\n                type,\n                geometry {\n                    type,\n                    coordinates\n                },\n                properties {\n                    id,\n                    iconCategory,\n                    magnitudeOfDelay,\n                    events {\n                        description,\n                        code,\n                        iconCategory\n                    },\n                    startTime,\n                    endTime,\n                    from,\n                    to,\n                    length,\n                    delay,\n                    roadNumbers,\n                    aci {\n                        probabilityOfOccurrence,\n                        numberOfReports,\n                        lastReportTime\n                    }\n                }\n            }\n        }",
+    };
+    return this.services.incidentDetailsV5(e);
+  }),
+  (IncidentsDetailsManager.prototype._registerEvents = function () {
+    var e = window.debounce(
+      function (e) {
+        "vectorTilesIncidents" === e.sourceId && this._updateIncidentMarkers();
+      }.bind(this),
+      500
+    );
+    this.map.on("sourcedata", e),
+      this.map.on("moveend", this._updateIncidentMarkers),
+      this.map.on("zoomend", this._updateIncidentMarkers);
+  }),
+  (IncidentsDetailsManager.prototype._updateIncidentMarkers = function () {
+    this.map.getZoom() > 9 &&
+      this._fetchIncidentDetails().then(
+        function (e) {
+          this.incidentsMarkerManager.addMarkers(e),
+            this.onDetailsUpdated({
+              trafficIncidents: e,
+              markers: this.incidentsMarkerManager.getMarkers(),
+            });
+        }.bind(this)
+      );
+  }),
+  (window.IncidentsDetailsManager =
+    window.IncidentsDetailsManager || IncidentsDetailsManager);
